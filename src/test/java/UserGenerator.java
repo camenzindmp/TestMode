204499857785b9126d131864c540e7901f4005fc
Максим Пенкин <m.penkin@gmail.com>
2020-11-11 08:32:30 +1000

    interim commit

--- src/test/java/UserGenerator.java
+++ src/test/java/UserGenerator.java
@@ -2,33 +2,31 @@ import io.restassured.builder.RequestSpecBuilder;
 import io.restassured.filter.log.LogDetail;
 import io.restassured.specification.RequestSpecification;
 import jdk.jfr.ContentType;
+import jdk.jfr.DataAmount;
+import lombok.AllArgsConstructor;
+import lombok.Data;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.*
 import static io.restassured.matcher.RestAssuredMatchers.*
 import static org.hamcrest.Matchers.*
 
 public class UserGenerator {
- private static RequestSpecification requestSpec = new RequestSpecBuilder()
-         .setBaseUri("http://localhost")
-         .setPort(9999)
-         .setAccept(ContentType.JSON)
-         .setContentType(ContentType.JSON)
-         .log(LogDetail.ALL)
-         .build();
+    private UserGenerator() {
+    }
 
- @BeforeAll
- static void setUpAll() {
-  // сам запрос
-  given()
-          .spec(requestSpec) // указываем, какую спецификацию используем
-          .body(new RegistrationDto("vasya", "password", "active")) // передаём в теле объект, который будет преобразован в JSON
-          .when() // "когда"
-          .post("/api/system/users") // на какой путь, относительно BaseUri отправляем запрос
-          .then() // "тогда ожидаем"
-          .statusCode(200); // код 200 OK
- }
-    ...
+    @Data
+    @AllArgsConstructor
+    public static class RegistrationDto {
+        String name;
+        String password;
+        String status;
+    }
+
+    private static getRegistrationDto RegistrationDto() {
+         return new RegistrationDto();
+    }
 }
 
 // java -jar app-ibank.jar -P:profile=test
--- src/test/java/UserTest.java
+++ src/test/java/UserTest.java
@@ -1,2 +1,39 @@
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.filter.log.LogDetail;
+import io.restassured.specification.RequestSpecification;
+import jdk.jfr.ContentType;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.*
+import static io.restassured.matcher.RestAssuredMatchers.*
+import static org.hamcrest.Matchers.*
+
 public class UserTest {
+    private static RequestSpecification requestSpec = new RequestSpecBuilder()
+            .setBaseUri("http://localhost")
+            .setPort(9999)
+            .setAccept(ContentType.JSON)
+            .setContentType(ContentType.JSON)
+            .log(LogDetail.ALL)
+            .build();
+
+    @BeforeAll
+    static void setUpAll() {
+        // сам запрос
+        given()
+                .spec(requestSpec) // указываем, какую спецификацию используем
+                .body(new UserGenerator.RegistrationDto("vasya", "password", "active")) // передаём в теле объект, который будет преобразован в JSON
+                .when() // "когда"
+                .post("/api/system/users") // на какой путь, относительно BaseUri отправляем запрос
+                .then() // "тогда ожидаем"
+                .statusCode(200); // код 200 OK
+    }
+
+    @Test
+    void test() {
+
+    }
 }
+
+// java -jar app-ibank.jar -P:profile=test

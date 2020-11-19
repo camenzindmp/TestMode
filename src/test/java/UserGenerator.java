b7577a6e30a32797394984f72dce3af8a69f025e
camenzindmp <camenzindmp@gmail.com>
2020-11-19 16:50:16 +1000

    UserGenerator.java refactor

--- src/test/java/UserGenerator.java
+++ src/test/java/UserGenerator.java
@@ -2,6 +2,8 @@ import io.restassured.builder.RequestSpecBuilder;
 import io.restassured.filter.log.LogDetail;
 import io.restassured.specification.RequestSpecification;
 import jdk.jfr.ContentType;
+import lombok.AllArgsConstructor;
+import lombok.Data;
 import org.junit.jupiter.api.BeforeAll;
 
 import static io.restassured.RestAssured.*
@@ -9,26 +11,39 @@ import static io.restassured.matcher.RestAssuredMatchers.*
 import static org.hamcrest.Matchers.*
 
 public class UserGenerator {
- private static RequestSpecification requestSpec = new RequestSpecBuilder()
-         .setBaseUri("http://localhost")
-         .setPort(9999)
-         .setAccept(ContentType.JSON)
-         .setContentType(ContentType.JSON)
-         .log(LogDetail.ALL)
-         .build();
+    private UserGenerator() {
+    }
 
- @BeforeAll
- static void setUpAll() {
-  // сам запрос
-  given()
-          .spec(requestSpec) // указываем, какую спецификацию используем
-          .body(new RegistrationDto("vasya", "password", "active")) // передаём в теле объект, который будет преобразован в JSON
-          .when() // "когда"
-          .post("/api/system/users") // на какой путь, относительно BaseUri отправляем запрос
-          .then() // "тогда ожидаем"
-          .statusCode(200); // код 200 OK
- }
-    ...
+    private static RequestSpecification requestSpec = new RequestSpecBuilder()
+            .setBaseUri("http://localhost")
+            .setPort(9999)
+            .setAccept(ContentType.JSON)
+            .setContentType(ContentType.JSON)
+            .log(LogDetail.ALL)
+            .build();
+
+    @BeforeAll
+    static void setUpAll() {
+        // сам запрос
+        given()
+                .spec(requestSpec) // указываем, какую спецификацию используем
+                .body(new RegistrationDto("vasya", "password", "active")) // передаём в теле объект, который будет преобразован в JSON
+                .when() // "когда"
+                .post("/api/system/users") // на какой путь, относительно BaseUri отправляем запрос
+                .then() // "тогда ожидаем"
+                .statusCode(200); // код 200 OK
+    }
+
+    @Data
+    @AllArgsConstructor
+    public static class RegistrationDto {
+        private String name;
+        private String password;
+        private String status;
+    }
+
+    private static RegistrationDto getRegistrationDto() {
+        return new RegistrationDto("vasya", "password", "active");
+    }
 }
 
-// java -jar app-ibank.jar -P:profile=test
